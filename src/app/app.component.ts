633f1b23304fff519184cbc346e09bb551317d95
pavanpagan <ahitisham.a@gmail.com>
2021-01-13 21:38:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { lorem } from 'faker'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing';
+  //getting random sententece from faker library
+  randomText = lorem.sentence();
+  enteredText = '';
+
+  //Handle user input
+  onInput(value:string) {
+    this.enteredText = value; 
+  }
+  compare(randomLetter:string, enteredLetter:string) {
+    if(!enteredLetter) {
+      return 'pending';
+    } 
+     
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
+  reLoad() {
+    location.reload();
+  }
 }
